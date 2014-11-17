378584d1220be1255f8a3b95ee733ca01e1d95f9
Luis Orozco <lcorozco10@gmail.com>
2014-11-17 16:19:52 -0600

    Tablas nuevas desde Zend Studio

--- index.php
+++ index.php
@@ -2,6 +2,46 @@
 class mainClass {
     function __construct() {
         echo("Hola Mundo3");
-        
+
     }
 }
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Document</title>
+</head>
+<body>
+<table>
+	<tbody>
+		<td>
+			<tr><a href=""><span href="#">columna 1</span></a></tr>
+			<tr><a href=""><span href="#">columna 2</span></a></tr>
+			<tr><a href=""><span href="#">columna 3</span></a></tr>
+		</td>
+		<td>
+			<tr><a href=""><span href="#">columna 1</span></a></tr>
+			<tr><a href=""><span href="#">columna 2</span></a></tr>
+			<tr><a href=""><span href="#">columna 3</span></a></tr>
+		</td>
+		<td>
+			<tr><a href=""><span href="#">columna 1</span></a></tr>
+			<tr><a href=""><span href="#">columna 2</span></a></tr>
+			<tr><a href=""><span href="#">columna 3</span></a></tr>
+		</td>
+		<td>
+			<tr><a href=""><span href="#">columna 1</span></a></tr>
+			<tr><a href=""><span href="#">columna 2</span></a></tr>
+			<tr><a href=""><span href="#">columna 3</span></a></tr>
+		</td>
+		<td>
+			<tr><a href=""><span href="#">columna 1</span></a></tr>
+			<tr><a href=""><span href="#">columna 2</span></a></tr>
+			<tr><a href=""><span href="#">columna 3</span></a></tr>
+		</td>
+	</tbody>
+</table>
+</body>
+</html>
