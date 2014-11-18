e80791dcfbb75fbe147a79fa43a7f06579bf4b7a
Luis Orozco <lcorozco10@gmail.com>
2014-11-18 10:10:27 -0600

    Nueva Clase Class2.php

---
+++ Class2.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author COCOMSYS1
+ *
+ */
+class Class2 {
+	// TODO - Insert your code here
+
+	/**
+	 */
+	function __construct() {
+
+		// TODO - Insert your code here
+	}
+}
+
+?>
