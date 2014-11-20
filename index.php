1c5335aa6ce4fc9ea3cf37b4049c0d3e63114603
Luis Orozco <lcorozco10@gmail.com>
2014-11-20 10:25:18 -0600

    Add CSS and JS Foldels

--- index.php
+++ index.php
@@ -10,45 +10,25 @@ class mainClass {
     }
 }
 //Comentario
-$obj = new Class1('Ejemplo');
+/* $obj = new Class1('Ejemplo');
 $var = $obj->getdato();
-echo $var;
+echo $var; */
 ?>
+
 <!doctype html>
-<html lang="en">
+<html lang="en" ng-app>
 <head>
 	<meta charset="UTF-8" />
-	<title>Inicio</title>
+	<title>JS Angular Examples</title>
 </head>
-<body>
-<table>
-	<tbody>
-		<tr>
-			<td><a href="#">Columna 1</a></td>
-			<td><a href="#">Columna 2</a></td>
-			<td><a href="#">Columna 3</a></td>
-		</tr>
-		<tr>
-			<td><a href="#">Columna 1</a></td>
-			<td><a href="#">Columna 2</a></td>
-			<td><a href="#">Columna 3</a></td>
-		</tr>
-		<tr>
-			<td><a href="#">Columna 1</a></td>
-			<td><a href="#">Columna 2</a></td>
-			<td><a href="#">Columna 3</a></td>
-		</tr>
-		<tr>
-			<td><a href="#">Columna 1</a></td>
-			<td><a href="#">Columna 2</a></td>
-			<td><a href="#">Columna 3</a></td>
-		</tr>
-		<tr>
-			<td><a href="#">Columna 1</a></td>
-			<td><a href="#">Columna 2</a></td>
-			<td><a href="#">Columna 3</a></td>
-		</tr>
-	</tbody>
-</table>
-</body>
+	<body>
+		<div>
+			<label>Name:</label>
+	     	<input type="text" ng-model="yourName" placeholder="Enter a name here">
+	     	<hr>
+	      	<h1>Hello {{yourName}}!</h1>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.3/angular.min.js"></script>
+		<script src="js/jsAngularController.js"></script>
+	</body>
 </html>
